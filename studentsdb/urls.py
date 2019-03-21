10540cfcd21ebce0a303469b7f0877a50f58a295
bogdan karpus <bigkrp@gmail.com>
2019-03-21 14:10:03 +0200

    Was added context vars

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
     # Groups list
     url(r'^groups/$', views.groups_list, name='groups'),
-    url(r'^groups/add/$', views.groups_add, name='groups_add'),    
+    url(r'^groups/add/$', views.groups_add, name='groups_add'),
     url(r'^groups/(?P<gid>\d+)/edit$', views.groups_edit, name='groups_edit'),
     url(r'^groups/(?P<gid>\d+)/delete$', views.groups_delete, name='groups_delete'),
 
